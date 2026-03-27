3b2049c2e7536a948c39ea126865bed93fd4f863
prit1626 <pritchovatiya1626@gmail.com>
2026-03-27 14:59:13 +0530

    Initial commit

---
+++ backend/src/main/java/com/ayurvault/AyurvaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ayurvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyurvaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyurvaultBackendApplication.class, args);
+	}
+
+}
